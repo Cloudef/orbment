c4ec929fb79b9ead6ff0e30a2b24fcb54ea1adb7
Jari Vetoniemi <mailroxas@gmail.com>
2014-08-04 23:41:22 +0300

    Update wlc, and add some basic tiling.

--- src/loliwm.c
+++ src/loliwm.c
@@ -6,21 +6,75 @@
 #include <assert.h>
 
 #include <wlc.h>
+#include <wayland-util.h>
 
 static struct {
    struct wlc_compositor *compositor;
+   struct wlc_view *active;
+   struct wl_list views;
 } loliwm;
+
+static void
+relayout(void)
+{
+   struct wlc_view *v;
+   bool toggle = false;
+   uint32_t count = wl_list_length(&loliwm.views);
+   uint32_t y = 0, height = 480 / (count > 1 ? count - 1 : 1);
+   wlc_view_for_each(v, &loliwm.views) {
+      wlc_view_set_state(v, (uint32_t[]){ WLC_MAXIMIZED }, 1);
+      wlc_view_resize(v, (count > 1 ? 400 : 800), (toggle ? height : 480));
+      wlc_view_position(v, (toggle ? 400 : 0), y);
+
+      if (toggle)
+         y += height;
+
+      toggle = true;
+   }
+}
+
+static void
+set_active(struct wlc_view *view)
+{
+   if (loliwm.active == view)
+      return;
+
+   if (loliwm.active)
+      wlc_view_set_active(loliwm.active, false);
+
+   if (view)
+      wlc_view_set_active(view, true);
+
+   loliwm.active = view;
+}
 
 static void
 view_created(struct wlc_compositor *compositor, struct wlc_view *view)
 {
    (void)compositor;
+   wl_list_insert((wl_list_length(&loliwm.views) > 0 ? loliwm.views.prev : &loliwm.views), wlc_view_get_link(view));
+   set_active(view);
+   relayout();
+}
 
-   static int c = 0;
-   wlc_view_set_state(view, (uint32_t[]){ WLC_MAXIMIZED }, 1);
-   wlc_view_resize(view, 400, 480);
-   wlc_view_position(view, (c ? 400 : 0), 0);
-   c = !c;
+static void
+view_destroyed(struct wlc_compositor *compositor, struct wlc_view *view)
+{
+   (void)compositor;
+
+   wl_list_remove(wlc_view_get_link(view));
+
+   if (loliwm.active == view) {
+      loliwm.active = NULL;
+
+      struct wlc_view *v;
+      if (wl_list_length(&loliwm.views) > 0 && (v = wlc_view_from_link(loliwm.views.prev))) {
+         wlc_compositor_keyboard_focus(compositor, v);
+         set_active(v);
+      }
+   }
+
+   relayout();
 }
 
 static bool
@@ -28,18 +82,24 @@ button_press(struct wlc_compositor *compositor, struct wlc_view *view, uint32_t 
 {
    (void)button;
 
-   if (state == WLC_BUTTON_STATE_PRESSED && button == 273) {
+   if (state == WLC_BUTTON_STATE_RELEASED && button == 273) {
       // TEMPORARY UGLY
       if (fork() == 0) {
          system("weston-terminal");
          _exit(0);
       }
+   } else if (state == WLC_BUTTON_STATE_RELEASED) {
+      wlc_compositor_keyboard_focus(compositor, view);
+      set_active(view);
    }
 
-   if (state == WLC_BUTTON_STATE_RELEASED)
-      wlc_compositor_keyboard_focus(compositor, view);
-
    return true;
+}
+
+static void
+keyboard_init(struct wlc_compositor *compositor, struct wlc_view *view)
+{
+   wlc_compositor_keyboard_focus(compositor, view);
 }
 
 static void
@@ -60,7 +120,7 @@ initialize(void)
    struct wlc_interface interface = {
       .view = {
          .created = view_created,
-         .destroyed = NULL,
+         .destroyed = view_destroyed,
       },
 
       .pointer = {
@@ -69,11 +129,13 @@ initialize(void)
       },
 
       .keyboard = {
+         .init = keyboard_init,
          .key = NULL,
       },
    };
 
    wlc_compositor_inject(loliwm.compositor, &interface);
+   wl_list_init(&loliwm.views);
    return true;
 
 fail:
