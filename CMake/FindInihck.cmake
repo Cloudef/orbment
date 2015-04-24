e82ab8fc5ce194846122fb359a82c05be90368f0
Jari Vetoniemi <mailroxas@gmail.com>
2015-04-24 22:39:01 +0300

    Make all find modules use PkgConfig and make stuff more consistent

--- CMake/FindInihck.cmake
+++ CMake/FindInihck.cmake
@@ -2,23 +2,19 @@
 # Find the inihck libraries
 #
 #  This module defines the following variables:
-#     INIHCK_FOUND        - true if INIHCK_INCLUDE_DIR & INIHCK_LIBRARY are found
-#     INIHCK_LIBRARIES    - Set when INIHCK_LIBRARY is found
-#     INIHCK_INCLUDE_DIRS - Set when INIHCK_INCLUDE_DIR is found
-#
-#     INIHCK_INCLUDE_DIR  - where to find inihck.h, etc.
-#     INIHCK_LIBRARY      - the inihck library
+#     INIHCK_FOUND        - True if inihck is found
+#     INIHCK_LIBRARIES    - inihck libraries
+#     INIHCK_INCLUDE_DIRS - inihck include directories
+#     INIHCK_DEFINITIONS  - Compiler switches required for using inihck
 #
 
-find_path(INIHCK_INCLUDE_DIR NAMES inihck/inihck.h)
-find_library(INIHCK_LIBRARY NAMES inihck)
+find_package(PkgConfig)
+pkg_check_modules(PC_INIHCK QUIET inihck)
+find_path(INIHCK_INCLUDE_DIRS NAMES inihck/inihck.h HINTS ${PC_INIHCK_INCLUDE_DIRS})
+find_library(INIHCK_LIBRARIES NAMES inihck HINTS ${PC_INIHCK_LIBRARY_DIRS})
+
+set(INIHCK_DEFINITIONS ${PC_INIHCK_CFLAGS_OTHER})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(inihck DEFAULT_MSG INIHCK_LIBRARY INIHCK_INCLUDE_DIR)
-
-if (INIHCK_FOUND)
-   set(INIHCK_LIBRARIES ${INIHCK_LIBRARY})
-   set(INIHCK_INCLUDE_DIRS ${INIHCK_INCLUDE_DIR})
-endif ()
-
-mark_as_advanced(INIHCK_INCLUDE_DIR INIHCK_LIBRARY)
+find_package_handle_standard_args(inihck DEFAULT_MSG INIHCK_LIBRARIES INIHCK_INCLUDE_DIRS)
+mark_as_advanced(INIHCK_LIBRARIES INIHCK_INCLUDE_DIRS)
--- CMake/FindWLC.cmake
+++ CMake/FindWLC.cmake
@@ -2,23 +2,19 @@
 # Find the wlc libraries
 #
 #  This module defines the following variables:
-#     WLC_FOUND        - true if WLC_INCLUDE_DIR & WLC_LIBRARY are found
-#     WLC_LIBRARIES    - Set when WLC_LIBRARY is found
-#     WLC_INCLUDE_DIRS - Set when WLC_INCLUDE_DIR is found
-#
-#     WLC_INCLUDE_DIR  - where to find wlc.h, etc.
-#     WLC_LIBRARY      - the wlc library
+#     WLC_FOUND        - True if wlc is found
+#     WLC_LIBRARIES    - wlc libraries
+#     WLC_INCLUDE_DIRS - wlc include directories
+#     WLC_DEFINITIONS  - Compiler switches required for using wlc
 #
 
-find_path(WLC_INCLUDE_DIR NAMES wlc/wlc.h)
-find_library(WLC_LIBRARY NAMES wlc)
+find_package(PkgConfig)
+pkg_check_modules(PC_WLC QUIET wlc)
+find_path(WLC_INCLUDE_DIRS NAMES wlc/wlc.h HINTS ${PC_WLC_INCLUDE_DIRS})
+find_library(WLC_LIBRARIES NAMES wlc HINTS ${PC_WLC_LIBRARY_DIRS})
+
+set(WLC_DEFINITIONS ${PC_WLC_CFLAGS_OTHER})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(wlc DEFAULT_MSG WLC_LIBRARY WLC_INCLUDE_DIR)
-
-if (WLC_FOUND)
-   set(WLC_LIBRARIES ${WLC_LIBRARY})
-   set(WLC_INCLUDE_DIRS ${WLC_INCLUDE_DIR})
-endif ()
-
-mark_as_advanced(WLC_INCLUDE_DIR WLC_LIBRARY)
+find_package_handle_standard_args(wlc DEFAULT_MSG WLC_LIBRARIES WLC_INCLUDE_DIRS)
+mark_as_advanced(WLC_LIBRARIES WLC_INCLUDE_DIRS)
